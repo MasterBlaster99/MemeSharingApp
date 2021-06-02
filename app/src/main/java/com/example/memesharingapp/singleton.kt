b9ba927b773a5228fd5d863d71712e0d0acb6764
Shekhar Shinde <shekharshinde9990@gmail.com>
2021-06-02 19:11:54 +0530

    all files added

---
+++ app/src/main/java/com/example/memesharingapp/singleton.kt
@@ -0,0 +1,28 @@
+package com.example.memesharingapp
+
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+class singleton constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var INSTANCE: singleton? = null
+        fun getInstance(context: Context) =
+                INSTANCE ?: synchronized(this) {
+                    INSTANCE ?: singleton(context).also {
+                        INSTANCE = it
+                    }
+                }
+    }
+
+    private val requestQueue: RequestQueue by lazy {
+        // applicationContext is key, it keeps you from leaking the
+        // Activity or BroadcastReceiver if someone passes one in.
+        Volley.newRequestQueue(context.applicationContext)
+    }
+    fun <T> addToRequestQueue(req: Request<T>) {
+        requestQueue.add(req)
+    }
+}
